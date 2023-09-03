911c7f1be8a99795d3549a50c91452e37a2d1bac
이재원 <nani6765@doublemedia.co.kr>
2023-09-03 13:04:52 +0900

    [TEST] React Navite WebView Test

--- App.tsx
+++ App.tsx
@@ -6,17 +6,32 @@ import {
 } from '@react-navigation/native-stack';
 import { Button } from 'react-native';
 
-import HomePage from './screens/HomeScreen';
-import WebViewPage from './screens/WebViewScreen';
+import WebViewScreen from './screens/WebViewScreen';
+// import HomePage from './screens/HomeScreen';
+// import WebViewPage from './screens/WebViewScreen';
 // import { StyleSheet } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const HomeScreen = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iure
+      alias doloribus sapiente. Ipsa temporibus incidunt a exercitationem
+      voluptatibus libero explicabo inventore dolor? Nisi totam dignissimos
+      deleniti ipsum tempora ipsam.
+    </div>
+  );
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="bottom"></Stack.Navigator>
+      <Stack.Navigator initialRouteName="webView">
+        <Stack.Screen component={HomeScreen} name="home" />
+        <Stack.Screen component={WebViewScreen} name="webView" />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
